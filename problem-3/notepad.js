7d6143b19e81e471ec2114cd0c104819b08564a5
C18433856 <C18433856@mytudublin.ie>
2022-12-04 16:18:26 +0000

    Making custom element work

--- problem-3/notepad.js
+++ problem-3/notepad.js
@@ -1,12 +1,16 @@
 const Observable = rxjs;
 
-Observable.fromEvent(document.getElementById("add"), 'click')
-  .subscribe(() => Add());
+//Observable.fromEvent(document.getElementById("add"), 'click')
+//  .subscribe(() => Add());
 
 class notepadNote extends HTMLElement{
-  connectedCallback(){
-    this.innerHTML = "<h1>This is a note</h1>";
+  constructor(){
+    super();
+    this.innerHTML = "This is a note"; 
   }
 }
 
 customElements.define("notepad-note", notepadNote);
+
+const test_note = new notepadNote;
+document.getElementById("list").appendChild(test_note);
