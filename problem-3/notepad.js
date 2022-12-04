78428c383f2d7946417371946e6e735ab97162ca
C18433856 <C18433856@mytudublin.ie>
2022-12-04 17:35:36 +0000

    Made a template

--- problem-3/notepad.js
+++ problem-3/notepad.js
@@ -3,10 +3,42 @@ const Observable = rxjs;
 //Observable.fromEvent(document.getElementById("add"), 'click')
 //  .subscribe(() => Add());
 
+const notepad_template = document.createElement('template');
+notepad_template.innerHTML = `
+<style>
+article{
+  text-align: center;
+  word-wrap: break-word;
+  width: 20vw;
+  margin: auto;
+  margin-top: 1vh;
+  color: white;
+  font-size: 3vh;
+}
+article p{
+  margin: 0;
+  background-color: blue;
+}
+#buttons{
+  text-align: center;
+  width: 20vw;
+  margin: auto;
+  font-size: 3vh;
+}</style>
+<article>
+  <p></p>
+  <div>
+    <button type="button">Delete</button>
+    <button type="button">Make Child Note</button>
+  </div>
+</article>`
+
+
 class notepadNote extends HTMLElement{
   constructor(){
     super();
-    this.innerHTML = "This is a note"; 
+    this.attachShadow({mode:open});
+    this.shadowRoot.appendChild(notepad_template.content.cloneNode(true));
   }
 }
 
