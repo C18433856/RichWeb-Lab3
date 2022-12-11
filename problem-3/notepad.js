bc244cab7add6c46e721c6a7f8a23bf1f6d5ac88
C18433856 <C18433856@mytudublin.ie>
2022-12-11 21:45:05 +0000

    Made children notes display under parent notes, changed styling

--- problem-3/notepad.js
+++ problem-3/notepad.js
@@ -2,7 +2,7 @@ const Observable = rxjs;
 const {Subject} = rxjs;
 
 // Identify separate parent notes
-let unique_note = 0;
+let unique_note = 1;
 
 // Template for notepadNote class
 const notepad_template = document.createElement('template');
@@ -13,24 +13,19 @@ article{
   word-wrap: break-word;
   width: 20vw;
   margin: auto;
-  margin-top: 1vh;
+  margin-top: 2vh;
   font-size: 3vh;
   border: 1px solid black;
 }
 article p{
   margin: 0;
 }
-#buttons{
-  text-align: center;
-  width: 20vw;
-  margin: auto;
-  font-size: 3vh;
-}
 #note-name{
   border-bottom: 1px solid black;
   background-color: lightgray;
-}</style>
-<article>
+}
+</style>
+<article id="note-body">
   <p id="note-name"></p>
   <p id="note-text"></p>
   <div>
@@ -50,15 +45,21 @@ class notepadNote extends HTMLElement{
     this.attachShadow({mode:"open"});
     this.shadowRoot.appendChild(notepad_template.content.cloneNode(true));
     this.shadowRoot.getElementById("note-text").innerText = text;
-    console.log(parent_in);
+
     // Make a child node
     if(parent_in){
       this.parent = parent_in;
       subject_in.subscribe({next: () => this.remove()})
+      this.shadowRoot.getElementById("note-name").style.backgroundColor = "#7393B3"
       this.shadowRoot.getElementById("note-name").innerText = "Child of " + this.parent.identity;
       this.shadowRoot.getElementById("child-create").style.display = "none";
-      this.shadowRoot.getElementById("note-delete").style.display = "none";
-      console.log(this.parent);
+      this.shadowRoot.getElementById("note-body").style.marginTop = "0.1px";
+      this.shadowRoot.getElementById("note-body").style.borderTop = 0;
+
+      // Allow note to delete itself using the delete button
+      Observable.fromEvent(this.shadowRoot.getElementById("note-delete"), 'click')
+      .subscribe(() => {this.remove()});
+
     }
     // Make a parent note
     else{
@@ -67,8 +68,12 @@ class notepadNote extends HTMLElement{
       unique_note++;
       this.shadowRoot.getElementById("note-name").innerText = this.identity;
       this.subject = new Subject(); // Child notes will subscribe to this
+
+      // Create child note
       Observable.fromEvent(this.shadowRoot.getElementById("child-create"), 'click')
       .subscribe(() => Add(prompt("Enter input for child node"),this, this.subject));
+
+      // When the delete button is clicked, delete all children than delete itself
       Observable.fromEvent(this.shadowRoot.getElementById("note-delete"), 'click')
       .subscribe(() => {this.subject.next(); this.remove()});
     }
@@ -77,12 +82,16 @@ class notepadNote extends HTMLElement{
 
 // Runs when the Add button is clicked
 function Add(input, parent=null, subject=null){
-  if(input == ""){
+  if(input == "" || input == null){
     alert("Please enter some text");
     return;
   }
   const note = new notepadNote(input, parent, subject);
-  document.getElementById("list").appendChild(note);
+  if(!parent)
+    document.getElementById("list").appendChild(note);
+  else
+    parent.insertAdjacentElement("afterend", note);
+  
 }
 
 // Define custom html element using the notepadNote class
